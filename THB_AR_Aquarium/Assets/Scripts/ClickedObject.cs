acaff41859c7681a46687f08dcd8b5394c1ead34
Blacksplay <Blacksplay.online@gmail.com>
2022-07-19 00:52:24 +0200

    Add visible Health on Model

--- THB_AR_Aquarium/Assets/Scripts/ClickedObject.cs
+++ THB_AR_Aquarium/Assets/Scripts/ClickedObject.cs
@@ -15,6 +15,11 @@ public class ClickedObject : MonoBehaviour
 
     public double changeovertime = 1;
 
+
+    public Material Farbe;
+
+    public Color color;
+
     void Update()
     {
         if(Health<0)
@@ -29,6 +34,19 @@ public class ClickedObject : MonoBehaviour
         if (hungry)
         {
             hunger();
+        }
+
+        if (Health < 25)
+        {
+            Farbe.color = Color.red;
+        }
+        if (Health < 75 && Health > 25)
+        {
+            Farbe.color = Color.yellow;
+        }
+        if (Health > 75)
+        {
+            Farbe.color = color;
         }
 
     }
